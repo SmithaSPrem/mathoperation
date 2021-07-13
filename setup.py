106d52513125debd3c562927b58db21ed899c0cb
User <user@ignitarium.com>
2021-07-13 13:46:33 -0400

    Edited setup file

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     version='0.1',
     description='Testing installation of Package',
     long_description=open('README.md').read(),
+    url = 'https://github.com/SmithaSPrem/mathoperation',
     author='Smitha',
     packages=['mathoperation'],
     install_requires=[])
