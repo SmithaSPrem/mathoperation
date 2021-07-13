9a0c2fb337e3c0f88c971a01932a2db17d1bf9ef
User <user@ignitarium.com>
2021-07-13 13:41:58 -0400

    Added a project to create pacakage

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mathoperation',
+    version='0.1',
+    description='Testing installation of Package',
+    long_description=open('README.md').read(),
+    author='Smitha',
+    packages=['mathoperation'],
+    install_requires=[])
